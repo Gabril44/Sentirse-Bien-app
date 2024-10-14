d7372dddedc84b3ddedc67b138c785ffa884d68c
EnzoDurini <enzodurini@gmail.com>
2024-10-13 21:29:13 -0300

    13-10 21:28 menu nuevo

--- SentirseBien/Index.Designer.cs
+++ SentirseBien/Index.Designer.cs
@@ -42,7 +42,35 @@
             welcome_label = new Label();
             label1 = new Label();
             rol_label = new Label();
+            PanelMenu = new Panel();
+            pictureBox1 = new PictureBox();
+            panel5 = new Panel();
+            labelcerrar = new Label();
+            panelPerfil = new Panel();
+            labelPerfil = new Label();
+            panelPagos = new Panel();
+            label5 = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            PanelButtomClientes = new Panel();
+            label2 = new Label();
+            panelPerfilEditar = new Panel();
+            labelPerfilEditar = new Label();
+            label6 = new Label();
+            panelPerfilCerrar = new Panel();
+            labelPerfilCerrar = new Label();
+            panelMenuPerfil = new Panel();
             menuStrip1.SuspendLayout();
+            PanelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel5.SuspendLayout();
+            panelPerfil.SuspendLayout();
+            panelPagos.SuspendLayout();
+            panel2.SuspendLayout();
+            PanelButtomClientes.SuspendLayout();
+            panelPerfilEditar.SuspendLayout();
+            panelPerfilCerrar.SuspendLayout();
+            panelMenuPerfil.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
@@ -55,6 +83,7 @@
             menuStrip1.Size = new Size(81, 450);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked_1;
             // 
             // clienteToolStripMenuItem
             // 
@@ -143,12 +172,220 @@
             rol_label.TabIndex = 3;
             rol_label.Text = "label2";
             // 
+            // PanelMenu
+            // 
+            PanelMenu.BackColor = Color.FromArgb(29, 56, 49);
+            PanelMenu.Controls.Add(pictureBox1);
+            PanelMenu.Controls.Add(panel5);
+            PanelMenu.Controls.Add(panelPerfil);
+            PanelMenu.Controls.Add(panelPagos);
+            PanelMenu.Controls.Add(panel2);
+            PanelMenu.Controls.Add(PanelButtomClientes);
+            PanelMenu.Dock = DockStyle.Left;
+            PanelMenu.Font = new Font("SansSerif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 2);
+            PanelMenu.Location = new Point(81, 0);
+            PanelMenu.Name = "PanelMenu";
+            PanelMenu.Size = new Size(170, 450);
+            PanelMenu.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.logo;
+            pictureBox1.Location = new Point(24, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(129, 78);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // panel5
+            // 
+            panel5.AutoSize = true;
+            panel5.BorderStyle = BorderStyle.FixedSingle;
+            panel5.Controls.Add(labelcerrar);
+            panel5.Dock = DockStyle.Bottom;
+            panel5.Location = new Point(0, 387);
+            panel5.Margin = new Padding(1, 1, 1, 20);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(170, 63);
+            panel5.TabIndex = 7;
+            panel5.MouseClick += panelCerrar_Click;
+            // 
+            // labelcerrar
+            // 
+            labelcerrar.AutoSize = true;
+            labelcerrar.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelcerrar.Location = new Point(45, 11);
+            labelcerrar.Margin = new Padding(1, 1, 1, 20);
+            labelcerrar.Name = "labelcerrar";
+            labelcerrar.Size = new Size(77, 30);
+            labelcerrar.TabIndex = 0;
+            labelcerrar.Text = "Cerrar";
+            labelcerrar.MouseClick += panelCerrar_Click;
+            // 
+            // panelPerfil
+            // 
+            panelPerfil.AutoSize = true;
+            panelPerfil.BorderStyle = BorderStyle.FixedSingle;
+            panelPerfil.Controls.Add(labelPerfil);
+            panelPerfil.Location = new Point(1, 279);
+            panelPerfil.Name = "panelPerfil";
+            panelPerfil.Size = new Size(170, 53);
+            panelPerfil.TabIndex = 6;
+            panelPerfil.ContextMenuStripChanged += cerrarSesiónToolStripMenuItem_Click;
+            panelPerfil.MouseClick += PanelPerfil_Click;
+            // 
+            // labelPerfil
+            // 
+            labelPerfil.AutoSize = true;
+            labelPerfil.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelPerfil.Location = new Point(46, 11);
+            labelPerfil.Name = "labelPerfil";
+            labelPerfil.Size = new Size(65, 30);
+            labelPerfil.TabIndex = 0;
+            labelPerfil.Text = "Perfil";
+            labelPerfil.MouseClick += PanelPerfil_Click;
+            // 
+            // panelPagos
+            // 
+            panelPagos.AutoSize = true;
+            panelPagos.BorderStyle = BorderStyle.FixedSingle;
+            panelPagos.Controls.Add(label5);
+            panelPagos.Location = new Point(0, 220);
+            panelPagos.Name = "panelPagos";
+            panelPagos.Size = new Size(170, 53);
+            panelPagos.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(42, 10);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 30);
+            label5.TabIndex = 0;
+            label5.Text = "Pagos";
+            // 
+            // panel2
+            // 
+            panel2.AutoSize = true;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(0, 161);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(170, 53);
+            panel2.TabIndex = 4;
+            panel2.MouseClick += turnosToolStripMenuItem_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(40, 11);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 30);
+            label3.TabIndex = 0;
+            label3.Text = "Turnos";
+            label3.MouseClick += turnosToolStripMenuItem_Click;
+            // 
+            // PanelButtomClientes
+            // 
+            PanelButtomClientes.AutoSize = true;
+            PanelButtomClientes.BorderStyle = BorderStyle.FixedSingle;
+            PanelButtomClientes.Controls.Add(label2);
+            PanelButtomClientes.Location = new Point(0, 102);
+            PanelButtomClientes.Name = "PanelButtomClientes";
+            PanelButtomClientes.Size = new Size(170, 53);
+            PanelButtomClientes.TabIndex = 0;
+            PanelButtomClientes.Paint += panel2_Paint;
+            PanelButtomClientes.MouseClick += clienteToolStripMenuItem_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(34, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 30);
+            label2.TabIndex = 0;
+            label2.Text = "Clientes";
+            label2.Click += label2_Click;
+            label2.MouseClick += clienteToolStripMenuItem_Click;
+            // 
+            // panelPerfilEditar
+            // 
+            panelPerfilEditar.AutoSize = true;
+            panelPerfilEditar.BackColor = Color.FromArgb(29, 56, 49);
+            panelPerfilEditar.BorderStyle = BorderStyle.FixedSingle;
+            panelPerfilEditar.Controls.Add(labelPerfilEditar);
+            panelPerfilEditar.Controls.Add(label6);
+            panelPerfilEditar.Location = new Point(3, 3);
+            panelPerfilEditar.Name = "panelPerfilEditar";
+            panelPerfilEditar.Size = new Size(170, 36);
+            panelPerfilEditar.TabIndex = 7;
+            panelPerfilEditar.Paint += panel1_Paint;
+            // 
+            // labelPerfilEditar
+            // 
+            labelPerfilEditar.AutoSize = true;
+            labelPerfilEditar.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelPerfilEditar.Location = new Point(48, 2);
+            labelPerfilEditar.Name = "labelPerfilEditar";
+            labelPerfilEditar.Size = new Size(71, 30);
+            labelPerfilEditar.TabIndex = 1;
+            labelPerfilEditar.Text = "Editar";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(50, 1);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 30);
+            label6.TabIndex = 0;
+            // 
+            // panelPerfilCerrar
+            // 
+            panelPerfilCerrar.AutoSize = true;
+            panelPerfilCerrar.BackColor = Color.FromArgb(29, 56, 49);
+            panelPerfilCerrar.BorderStyle = BorderStyle.FixedSingle;
+            panelPerfilCerrar.Controls.Add(labelPerfilCerrar);
+            panelPerfilCerrar.Location = new Point(3, 45);
+            panelPerfilCerrar.Name = "panelPerfilCerrar";
+            panelPerfilCerrar.Size = new Size(170, 36);
+            panelPerfilCerrar.TabIndex = 8;
+            panelPerfilCerrar.MouseClick += cerrarSesiónToolStripMenuItem_Click;
+            // 
+            // labelPerfilCerrar
+            // 
+            labelPerfilCerrar.AutoSize = true;
+            labelPerfilCerrar.Font = new Font("Microsoft YaHei UI Light", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelPerfilCerrar.Location = new Point(5, 2);
+            labelPerfilCerrar.Name = "labelPerfilCerrar";
+            labelPerfilCerrar.Size = new Size(159, 30);
+            labelPerfilCerrar.TabIndex = 0;
+            labelPerfilCerrar.Text = "Cerrrar Sesión";
+            labelPerfilCerrar.MouseClick += cerrarSesiónToolStripMenuItem_Click;
+            // 
+            // panelMenuPerfil
+            // 
+            panelMenuPerfil.AutoSize = true;
+            panelMenuPerfil.BorderStyle = BorderStyle.FixedSingle;
+            panelMenuPerfil.Controls.Add(panelPerfilEditar);
+            panelMenuPerfil.Controls.Add(panelPerfilCerrar);
+            panelMenuPerfil.Location = new Point(268, 268);
+            panelMenuPerfil.Name = "panelMenuPerfil";
+            panelMenuPerfil.Size = new Size(178, 86);
+            panelMenuPerfil.TabIndex = 9;
+            // 
             // Index
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(72, 137, 121);
             ClientSize = new Size(931, 450);
+            Controls.Add(panelMenuPerfil);
+            Controls.Add(PanelMenu);
             Controls.Add(rol_label);
             Controls.Add(label1);
             Controls.Add(menuStrip1);
@@ -158,8 +395,29 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Index";
             WindowState = FormWindowState.Maximized;
+            Load += Index_Load;
+            MouseClick += index_Click;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            PanelMenu.ResumeLayout(false);
+            PanelMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panelPerfil.ResumeLayout(false);
+            panelPerfil.PerformLayout();
+            panelPagos.ResumeLayout(false);
+            panelPagos.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            PanelButtomClientes.ResumeLayout(false);
+            PanelButtomClientes.PerformLayout();
+            panelPerfilEditar.ResumeLayout(false);
+            panelPerfilEditar.PerformLayout();
+            panelPerfilCerrar.ResumeLayout(false);
+            panelPerfilCerrar.PerformLayout();
+            panelMenuPerfil.ResumeLayout(false);
+            panelMenuPerfil.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -180,5 +438,23 @@
         private Label rol_label;
         private ToolStripMenuItem comeCarneeToolStripMenuItem;
         private ToolStripMenuItem bueeeeToolStripMenuItem;
+        private Panel PanelMenu;
+        private Panel PanelButtomClientes;
+        private Label label2;
+        private Panel panelPerfil;
+        private Label labelPerfil;
+        private Panel panel2;
+        private Label label3;
+        private Panel panelPagos;
+        private Label label5;
+        private Panel panel5;
+        private Label labelcerrar;
+        private PictureBox pictureBox1;
+        private Panel panelPerfilEditar;
+        private Label label6;
+        private Panel panelPerfilCerrar;
+        private Label labelPerfilCerrar;
+        private Label labelPerfilEditar;
+        private Panel panelMenuPerfil;
     }
 }
--- SentirseBien/Index.cs
+++ SentirseBien/Index.cs
@@ -20,20 +20,21 @@ namespace SentirseBien
             //configurarMenuStrip(usuario);
             welcome_label.Text = usuario.nombre.ToString();
             getRol(usuario);
-            
+            panelMenuPerfil.Visible = false;
+
         }
-        private void getRol(Usuario usuario) 
+        private void getRol(Usuario usuario)
         {
             switch (usuario.rol)
             {
                 case 0:
-                    rol_label.Text = "Este usuario es un Cliente "+usuario.rol;
+                    rol_label.Text = "Este usuario es un Cliente " + usuario.rol;
                     break;
                 case 1:
-                    rol_label.Text = "Este usuario es un Empleado "+usuario.rol;
+                    rol_label.Text = "Este usuario es un Empleado " + usuario.rol;
                     break;
                 case 2:
-                    rol_label.Text = "Este usuario es un Secretario/a " +usuario.rol;
+                    rol_label.Text = "Este usuario es un Secretario/a " + usuario.rol;
                     break;
                 case 3:
                     rol_label.Text = "Este usuario es la Dra Felicidad!!!! omg!!! y tiene rol: " + usuario.rol;
@@ -83,6 +84,51 @@ namespace SentirseBien
             LoginForm loginForm = new LoginForm();
             this.Hide();
             loginForm.ShowDialog();
+        }
+
+
+        private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Index_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelPerfil_Click(object sender, EventArgs e)
+        {
+            panelMenuPerfil.Visible = !panelMenuPerfil.Visible;
+        }
+
+        private void panelCerrar_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void index_Click(object sender, MouseEventArgs e)
+        {
+           if( panelMenuPerfil.Visible)
+            {
+                panelMenuPerfil.Visible = false;
+            };
         }
     }
 }
